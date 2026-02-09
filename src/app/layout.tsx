b5e6a49f792c1dbd15def1f3d804983a5d2ffaa9
EmFa048 <maria.k.ljung@gmail.com>
2026-02-09 10:35:41 +0100

    Force refresh app icons with cache buster ?v=2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,8 +54,8 @@ export default function RootLayout({
   return (
     <html lang="sv" suppressHydrationWarning>
       <head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-72x72.png" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-72x72.png?v=2" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png?v=2" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5713849466989513" crossOrigin="anonymous"></script>
       </head>
       <body
